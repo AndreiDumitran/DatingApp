5a788347f528e422f9eb605070eb7a4ff6dc08cc
Dumitran Nicolae Andrei <dumitrandrei@Dumitrans-MacBook-Pro.local>
2023-01-09 17:06:15 +0200

    error handling api + client

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -25,7 +25,6 @@ export class NavComponent implements OnInit {
         this.router.navigateByUrl('/members');
         this.toastr.success('Login Success');
       },
-      error: (error) => this.toastr.error(error.error),
     });
   }
   logout() {
